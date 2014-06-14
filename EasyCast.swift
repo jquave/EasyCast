035185449b2e321fbfbc9f775c1af3e99e61a235
Jameson Quave <jquave@gmail.com>
2014-06-14 14:23:49 -0500

    Initial commit

---
+++ EasyCast.swift
@@ -0,0 +1,94 @@
+//
+//  EasyCast.swift
+//  SwiftIntrospection
+//
+//  Created by Jameson Quave on 6/14/14.
+//  Copyright (c) 2014 Jameson Quave. All rights reserved.
+//
+
+import Foundation
+
+func -(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) - Float(rhs)
+}
+func +(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) + Float(rhs)
+}
+func /(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) / Float(rhs)
+}
+func *(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) * Float(rhs)
+}
+func -(lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) - CGFloat(rhs)
+}
+func +(lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) + CGFloat(rhs)
+}
+func /(lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) / CGFloat(rhs)
+}
+func *(lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) * CGFloat(rhs)
+}
+func -(lhs: Float, rhs: Int) -> Float {
+    return Float(lhs) - Float(rhs)
+}
+func +(lhs: Float, rhs: Int) -> Float {
+    return Float(lhs) + Float(rhs)
+}
+func /(lhs: Float, rhs: Int) -> Float {
+    return Float(lhs) / Float(rhs)
+}
+func *(lhs: Float, rhs: Int) -> Float {
+    return Float(lhs) * Float(rhs)
+}
+func -(lhs: Float, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) - CGFloat(rhs)
+}
+func +(lhs: Float, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) + CGFloat(rhs)
+}
+func /(lhs: Float, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) / CGFloat(rhs)
+}
+func *(lhs: Float, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) * CGFloat(rhs)
+}
+func -(lhs: CGFloat, rhs: Int) -> CGFloat {
+    return CGFloat(lhs) - CGFloat(rhs)
+}
+func +(lhs: CGFloat, rhs: Int) -> CGFloat {
+    return CGFloat(lhs) + CGFloat(rhs)
+}
+func /(lhs: CGFloat, rhs: Int) -> CGFloat {
+    return CGFloat(lhs) / CGFloat(rhs)
+}
+func *(lhs: CGFloat, rhs: Int) -> CGFloat {
+    return CGFloat(lhs) * CGFloat(rhs)
+}
+func -(lhs: CGFloat, rhs: Float) -> CGFloat {
+    return CGFloat(lhs) - CGFloat(rhs)
+}
+func +(lhs: CGFloat, rhs: Float) -> CGFloat {
+    return CGFloat(lhs) + CGFloat(rhs)
+}
+func /(lhs: CGFloat, rhs: Float) -> CGFloat {
+    return CGFloat(lhs) / CGFloat(rhs)
+}
+func *(lhs: CGFloat, rhs: Float) -> CGFloat {
+    return CGFloat(lhs) * CGFloat(rhs)
+}
+func -(lhs: CGFloat, rhs: Double) -> CGFloat {
+    return CGFloat(lhs) - CGFloat(rhs)
+}
+func +(lhs: CGFloat, rhs: Double) -> CGFloat {
+    return CGFloat(lhs) + CGFloat(rhs)
+}
+func /(lhs: CGFloat, rhs: Double) -> CGFloat {
+    return CGFloat(lhs) / CGFloat(rhs)
+}
+func *(lhs: CGFloat, rhs: Double) -> CGFloat {
+    return CGFloat(lhs) * CGFloat(rhs)
+}
